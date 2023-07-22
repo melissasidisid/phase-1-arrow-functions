2d61386a1ae44db8766a3b4fe5554da66aee376b
melissa sidisaid <milyssasidisaid@gmail.com>
2023-07-22 17:37:22 +0200

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide (){
+    return 2000 / 100;
+    
+
+}
+
+const square = (parameter, parameter2) => parameter * parameter;
+
+
+   
+const add = (parameter, parameter2) => parameter + parameter2;
